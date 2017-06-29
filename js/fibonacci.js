52a6bde8b9d9f81ef9f905983e5e097057fef728
Jason Bourne <kiel835@gmail.com>
2017-06-29 13:08:07 +0000

    Initial

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,42 +1,33 @@
-var position = Number(prompt('Enter a number'));
 
-if (position === 0) {
-    document.write('Result: 0')
-} else if (position === 1) {
-    document.write('Result: 1')
-} else {
-    document.write('Result by cycle: ' + getFibonacciCycle(position) + '<br>');
-    document.write('Result by recursion: ' + recursion(position - 1, 0, 1) );
-}
+    var position = Number(prompt('Enter a number'));
 
-
-
-function getFibonacciCycle(n) {
-    var p1 = 0;
-    var p2 = 1;
-    var fibonacci;
-    for (var i = 2; i <= n; i++) {
-        fibonacci = p1 + p2;
-        p1 = p2;
-        p2 = fibonacci;
+    if (position === 0) {
+        document.write('Result: 0')
     }
-    return fibonacci;
-}
-
-       
-function recursion (num,f1,f2) {
-    if (num === 0) {
-        return f2;
+    else if (position === 1) {
+        document.write('Result: 1')
     }
-        return recursion(num-1, f2, f1 + f2);
-        
-}
+    else {
+        document.write('Result by cycle: ' + getFibonacciCycle(position, 0, 1) + '<br>');
+        document.write('Result by recursion: ' + recursion(position - 1, 0, 1));
+    }
+
+    
+    function getFibonacciCycle(n, p1, p2) {
+        var fibonacci;
+        for (var i = 2; i <= n; i++) {
+            fibonacci = p1 + p2;
+            p1 = p2;
+            p2 = fibonacci;
+        }
+        return fibonacci;
+    }
 
 
+    function recursion(num, f1, f2) {
+        if (num === 0) {
+            return f2;
+        }
+        return recursion(num - 1, f2, f1 + f2);
 
-
-
-
-
-
-
+    }
